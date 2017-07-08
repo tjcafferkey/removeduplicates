527a24a5830a1749555487e3ebacbedd105ce1b7
Tom Cafferky <tjcafferkey@gmail.com>
2017-07-08 13:05:11 +0100

    Check given parameters are correct

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 export default function removeDuplicates(arr, key) {
+    if (!(arr instanceof Array) || typeof key !== 'string' ) {
+        return false;
+    }
+
     return arr.filter((obj, index, arr) => {
         return arr.map(mapObj => mapObj[key]).indexOf(obj[key]) === index;
     });
