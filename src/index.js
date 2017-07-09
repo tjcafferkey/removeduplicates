852363a5b15598dfd6b306c26c8fa914f5772925
Tom Cafferky <tjcafferkey@gmail.com>
2017-07-09 13:21:38 +0100

    Remove duplicate items from an array

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 export default function removeDuplicates(arr, key) {
-    if (!(arr instanceof Array) || typeof key !== 'string' ) {
+    if (!(arr instanceof Array) || key && typeof key !== 'string') {
         return false;
     }
 
-    return arr.filter((obj, index, arr) => {
-        return arr.map(mapObj => mapObj[key]).indexOf(obj[key]) === index;
-    });
+    if (key && typeof key === 'string') {
+        return arr.filter((obj, index, arr) => {
+            return arr.map(mapObj => mapObj[key]).indexOf(obj[key]) === index;
+        });
+
+    } else {
+        return arr.filter(function(item, index, arr) {
+            return arr.indexOf(item) == index;
+        });
+    }
 }
